cdc70c445ac19ddd4c656eb8a039e336104e2b1a
Mubarak77-C <mubaraksache77@gmail.com>
2026-09-04 18:09:36 +0530

    "Prompt taking input and changes name by js function "

--- index.js
+++ index.js
@@ -14,29 +14,29 @@ if (age >= 18) {
     console.log("not elgible");
 }
 
-const pi=3.14;
+const pi = 3.14;
 // pi=10;
 console.log(pi); //3.14 come but due to pi=10 it will error thrown,  it fixed not changed 
 
 //Order of Operations (often remembered by acronyms like PEMDAS or BODMAS
-console.log((3 + 2) - 76 * (1 + 1)) ;
+console.log((3 + 2) - 76 * (1 + 1));
 
 console.log(23 + 97)
 
-console.log(23 + 97+2+4+3+11)
+console.log(23 + 97 + 2 + 4 + 3 + 11)
 console.log((4 + 6 + 9) / 77)
 
-let a=10;
+let a = 10;
 console.log(a);
-a=15;
+a = 15;
 console.log(a);
 
 let b = 7 * a
 console.log(b)
 
-const MAX=57;
-const actual=MAX-13;
-const percentage= actual/MAX;
+const MAX = 57;
+const actual = MAX - 13;
+const percentage = actual / MAX;
 console.log(percentage)
 
 let first_name = "john";
@@ -51,3 +51,14 @@ let combined = `${first_name} ${last_name}`
 document.querySelector(".content").textContent = combined; //this will overlap all .content class div p h1 overwite and print this john joe;
 
 document.getElementById("output").textContent = combined;
+
+// taking input button with click open promt
+function updateName(){
+    let name = prompt("Enter your Name");
+    button.textContent = `Plssayer 1: ${name}`;
+}
+
+
+
+const button = document.querySelector("button");
+button.addEventListener("click", updateName);
